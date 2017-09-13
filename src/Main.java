b9d1c21cd0c71aff8414a4d66b6fe0ceaed760f0
lakshmipriyaa <lakshmipriyavvit37@gmail.com>
2017-09-13 12:07:59 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+	      Student s=new Student();
+	      StudentGroup sg=new StudentGroup();
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
